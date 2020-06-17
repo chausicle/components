4cb61b2be40e96ccd2bbf999951af966ff4efbdf
Justin Chau <chausicle@gmail.com>
2020-06-16 20:02:40 -0700

    Create dummy data for different comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,22 +1,23 @@
 import React from "react";
-import faker from "faker";
 
 const Comment = (props) => {
+  const { avatar, name, comment, timeAgo } = props.data;
+
   return (
     <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
+      <a href="/" className="avatar">
+        <img alt="avatar" src={avatar} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {name}
         </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at {(new Date()).toLocaleTimeString()}</span>
-          </div>
-          <div className="text">Nice blog post!</div>
+        <div className="metadata">
+          <span className="date">{timeAgo}</span>
         </div>
+        <div className="text">{comment}</div>
       </div>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import faker from "faker";
 import Comment from "./components/Comment";
 
 const App = () => {
+  const commentDetails = [
+    {
+      avatar: faker.image.avatar(),
+      name: "Sam",
+      comment: "Nice blog post!",
+      timeAgo: "Today at 10:43PM",
+    },
+    {
+      avatar: faker.image.avatar(),
+      name: "Tom",
+      comment: "Looks good",
+      timeAgo: "Today at 7:55PM",
+    },
+    {
+      avatar: faker.image.avatar(),
+      name: "Dan",
+      comment: "Whoa!😮",
+      timeAgo: "Yesterday at 3:22PM",
+    },
+  ];
+
+  const handleCommentList = (comments) => {
+    return comments.map((detail, i) => <Comment key={i} data={detail} />);
+  };
+
   return (
     <div className="ui container comments">
-      <Comment />
+      {handleCommentList(commentDetails)}
     </div>
   );
 };
